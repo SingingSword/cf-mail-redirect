66e4521e07ce7e7e85c316c6a2ca51f3e6390c24
Enrico <enrico.brambilla@gmail.com>
2025-02-16 23:38:27 +0100

    checkpoint commit as of 2025-02-16 23:38:27 from 87.15.10.226

--- tf/redirect.tf
+++ tf/redirect.tf
@@ -16,6 +16,24 @@ resource "cloudflare_email_routing_rule" "spazzatura_rule" {
   enabled = true
 }
 
+resource "cloudflare_email_routing_rule" "garbage_rule" {
+  zone_id = var.cloudflare_zone_id
+  name = "garbage-redirect"
+
+  actions = [{
+    type = "forward"
+    value = [cloudflare_email_routing_address.spazzatura_address.email]
+  }]
+  matchers = [{
+    field = "to"
+    type = "literal"
+    value = "garbage@${var.domain}"
+  }]
+
+  priority = 0
+  enabled = true
+}
+
 resource "cloudflare_email_routing_address" "spazzatura_address" {
   account_id = var.cloudflare_account_id
   email = "brambrilliantwork+spazzatura@gmail.com"
